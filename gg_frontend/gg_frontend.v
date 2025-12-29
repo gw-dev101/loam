9ebbd159b46a46b3f430929686165e1af478287c
Linklancien <123779978+Linklancien@users.noreply.github.com>
2025-12-30 00:32:22 +0100

    Update gg_frontend.v

--- gg_frontend/gg_frontend.v
+++ gg_frontend/gg_frontend.v
@@ -3,10 +3,10 @@ module gg_frontend
 import gg
 import moduloam {RenderCommand}
 
-pub fn draw(mut g gg.Context, cmds []RenderCommand) {
+pub fn draw(mut gg_ctx gg.Context, cmds []RenderCommand) {
     for cmd in cmds {
         if cmd.command_type == .rectangle {
-            g.draw_rect_filled(
+            gg_ctx.draw_rect_filled(
                 cmd.bounds.x,
                 cmd.bounds.y,
                 cmd.bounds.width,
